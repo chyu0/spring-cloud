6860d1bfbf74a314014d18c05f6a507691acbc0e
wuchaowu <wuchaowu@tuhu.cn>
2019-05-13 17:46:54 +0800

    重构gateway

--- microservicecloud-gateway/src/main/java/com/wcw/demo/GatewayApplication.java
+++ microservicecloud-gateway/src/main/java/com/wcw/demo/GatewayApplication.java
@@ -2,37 +2,23 @@ package com.wcw.demo;
 
 
 
-import filter.MyAddRequestParameterGatewayFilterFactory;
-import filter.MyTokenFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@ComponentScan(value = {"com.wcw.demo.config"})
+
 public class GatewayApplication {
 
     @Value("${hostUrl}")
     private static String hostUrl;
 
-
-
-
     public static void main(String[] args) {
         System.out.println(hostUrl);
-
         SpringApplication.run(GatewayApplication.class, args);
-    }
-    @Bean
-    public MyTokenFilter tokenFilter() {
-        final MyTokenFilter myTokenFilter = new MyTokenFilter();
-        return myTokenFilter;
-    }
-    @Bean
-    public MyAddRequestParameterGatewayFilterFactory addRequestParameterGatewayFilterFactory(){
-        return new MyAddRequestParameterGatewayFilterFactory();
     }
 
 
